4705e32c5e3c9ffbb4e94307de5e13f60f6b98a1
Joao <joao.profilho77@gmail.com>
2026-01-08 20:51:37 -0300

    Ultima Atualizacao

--- src/main/java/com/joaojulioproenca/cliente/business/ClienteService.java
+++ src/main/java/com/joaojulioproenca/cliente/business/ClienteService.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+
 @Service
 @AllArgsConstructor
 public class ClienteService {
