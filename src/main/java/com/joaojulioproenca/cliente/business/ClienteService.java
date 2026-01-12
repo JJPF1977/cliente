4e8608b2eccee5b386f930f5aa395fd8a0a9ad93
Joao <joao.profilho77@gmail.com>
2026-01-12 19:26:23 -0300

    Atualizacao ClienteServise

--- src/main/java/com/joaojulioproenca/cliente/business/ClienteService.java
+++ src/main/java/com/joaojulioproenca/cliente/business/ClienteService.java
@@ -7,12 +7,13 @@ import com.joaojulioproenca.cliente.infrastructure.exceptions.ConflictException;
 import com.joaojulioproenca.cliente.infrastructure.exceptions.ResourceNotFoundException;
 import com.joaojulioproenca.cliente.infrastructure.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class ClienteService {
 
     private final ClienteRepository clienteRepository;
